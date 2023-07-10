cf5d38bcd67d21f6904e589d39a526a1d7b949ba
Daniel Grose <31761607+grosed@users.noreply.github.com>
2023-07-10 14:12:02 +0100

    Update matrix.exponential.py

--- parallel-processing/matrix.exponential/python/matrix.exponential.py
+++ parallel-processing/matrix.exponential/python/matrix.exponential.py
@@ -1,18 +1,14 @@
 import numpy as np
 import sys
 
-n = int(sys.argv[1])
-m = int(sys.argv[2])
-s = int(sys.argv[3])
+def matexp(X,m) :
+    n,_ = np.shape(X)
+    Z = np.identity(n)
+    Y = Z
+    for k in range(1,m) :
+        Z = np.matmul(Z,X)/k
+        Y = np.add(Y,Z)
+    return Y
 
-np.random.seed(s)
-
-X = np.random.rand(n,n)
-Z = np.identity(n)
-Y = Z
-
-for k in range(1,m) :
-    Z = np.matmul(Z,X)/k
-    Y = np.add(Y,Z)
 
 
