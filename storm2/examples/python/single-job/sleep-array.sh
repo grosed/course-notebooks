81a206040c7ddc350ed8f47836011cbcbbf53d3b
Daniel Grose <31761607+grosed@users.noreply.github.com>
2024-05-14 10:57:59 +0100

    Update sleep-array.sh

--- storm2/examples/python/single-job/sleep-array.sh
+++ storm2/examples/python/single-job/sleep-array.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH -J sleep-array-job
-#SBATCH --mail-user=dan.grose@lancaster.ac.uk
 #SBATCH --mail-type=ALL
+#SBATCH --mail-user=dan.grose@lancaster.ac.uk
 #SBATCH --array=10-120:10
 #SBATCH -c 1
 #SBATCH -o sleep-1-job_%A_%a.out
